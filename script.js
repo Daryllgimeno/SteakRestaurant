1ad6b5f11834144709b046922395e46895402aa4
Daryllgimeno <gimenodaryll@gmail.com>
2023-09-29 00:48:03 +0800

    Update

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById('card').addEventListener('submit', function(event) {
+      event.preventDefault(); 
+
+      var name = document.getElementById('name').value;
+      var contactNumber = document.getElementById('contactNumber').value;
+      var order = document.getElementById('order').value;
+
+     
+      console.log('Name:', name);
+      console.log('Contact Number:', contactNumber);
+      console.log('Order:', order);
+
+      document.getElementById('orderForm').reset();
+    });
